fe9f00b32f87534cbaf275187c464b841daeb1f1
geordidearns <geordidearns@gmail.com>
2015-04-04 19:26:45 +1300

    Added Balloon Kitteh files

---
+++ startbutton.js
@@ -0,0 +1,34 @@
+
+$(document).ready(function(e){
+
+      $('#startBtn').click(function(){
+
+        $('.playmusic').trigger('play');
+
+          var sec = 0;
+      function pad ( val ) { return val > 9 ? val : "0" + val; }
+      setInterval( function(){
+          $("#seconds").html(pad(++sec%60));
+          $("#minutes").html(pad(parseInt(sec/60,10)));
+      }, 1000);
+
+      $(this).remove();
+      });
+
+
+
+});
+
+ $('#muteBtn').click(function(){
+
+        $('.playmusic').trigger('pause');
+
+            var sec = 0;
+        function pad ( val ) { return val > 9 ? val : "0" + val; }
+        setInterval( function(){
+            $("#seconds").html(pad(++sec%60));
+            $("#minutes").html(pad(parseInt(sec/60,10)));
+        }, 1000);
+
+      $(this).remove();
+      });
